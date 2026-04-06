b693490bf7e236638c0af9626d626d2944581946
sri <srijanaad19@gmail.com>
2026-04-06 02:41:37 -0400

    added logic for registration

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -16,7 +16,24 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+  //invalidate if username or password is missing
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password required" });
+  }
+
+  let user = users.find(u => u.username === username && u.password === password);
+
+  if (!user) {
+    return res.status(404).json({ message: "No user matches found" })
+  }
+
+  const accessToken = jwt.sign({ username: user.username }, 'fingerprint_customer', { expiresIn: 60 * 60 })
+  req.session.authorization = { accessToken: accessToken }
+  
+  return res.status(200).json({ message: "Successfully registered user",
+                                token: accessToken })
 });
 
 // Add a book review
