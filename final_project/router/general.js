8a7ff52416dbf13f1e7d067da1d82f32754b50a4
sri <srijanaad19@gmail.com>
2026-04-06 03:46:49 -0400

    adding more routes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,13 +19,27 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+
+  if (!book) {
+    return res.status(404).json({ message: "no book found" })
+  } else {
+    return res.status(200).send(JSON.stringify(book));
+  } 
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const book = Object.entries(books).find(([_, val]) => val.author === author);
+  
+  if (!book) {
+    return res.status(404).json({ message: "no book found" })
+  } else {
+    return res.status(200).send(JSON.stringify(book));
+  } 
 });
 
 // Get all books based on title
